17fa81bf3a0bdb2b20947a9988ee68b30920c322
tanmayt <thapliyaltanmay@gmail.com>
2019-02-18 22:20:08 +0530

    add length checking and further modifications

--- stemmer.py
+++ stemmer.py
@@ -9,45 +9,36 @@ This is a developing script file.
 # dictionary = { 'अपनाना' : 'अपना', 'खेलना': 'खेल', 'खाना': 'खाना','सोना':'सो','सिल्ना':'सिल' ,'कुद्ना':'कुद ' ,'रखना': 'रख' ,'करना': 'कर', 'हसना': 'हस', 'समझना': 'समझ', 'बोलना': 'बोल', 'सुनना': 'सुन', 'देखना': 'देख', 'चाहना': 'चाह', 'मरना': 'मर', 'मारना' :'मार', 'देना': 'दे', 'लिखना': 'लिख', 'बनाना': 'बन', 'पढ़ना': 'पढ़', 'भूलना' :'भूल', 'खोलना': 'खोल', 'पटकना' :'पटक '} # Add the rest of your words and stems
 #stems = ['अपना', 'खेल', 'खाना','सो','सिल' ,'कुद ' , 'रख' , 'कर',  'हस',  'समझ',  'बोल',  'सुन',  'देख',  'चाह',  'मर', 'मार',  'दे',  'लिख',  'बन',  'पढ़', 'भूल', 'खोल', 'पटक','ओढ','बिछ','खोल','जल','डर','रह']
 sentence = input()
-flag=0
+flag1=0
 flag2=0
-counter=0
 #file=open("/home/tanmay/Desktop/Projects/Hindi_Stemmer/stems.txt","r")
 #lines=file.read().splitlines()
 #file1=open("/home/tanmay/Desktop/Projects/Hindi_Stemmer/ignorewrd.txt","r")
 #lines=file1.read().splitlines()
-file2=open("/home/tanmay/Desktop/Projects/Hindi_Stemmer/consonants.txt","r")
+file2=open("/home/tanmay/Desktop/Projects/Hindi_Stemmer/ignorewrd.txt","r")
 conso=file2.read().splitlines()
-lists=["ाएंगी","ाएंगे","ाऊंगी","ाऊंगा","ाइयाँ","ाइयों","ाइयां","ाएगी","ाएगा","ाओगी","ाओगे","एंगी","ेंगी","एंगे","ेंगे","ूंगी","ूंगा","ातीं","नाओं","नाएं","ताओं","ताएं","ियाँ","ियों","ियां","ाकर","ाइए","ाईं","ाया","ेगी","ेगा","ोगी","ोगे","ाने","ाना","ाते","ाती","ाता","तीं","ाओं","ाएं","ुओं","ुएं","ुआं","कर","ाओ","िए","ाई","ाए","ने","नी","ना","ते","ीं","ती","ता","ाँ","ां","ों","ें","ो","े","ू","ु","ी","ि","ा"]
+lists=["ाएंगी","ाएंगे","ाऊंगी","ाऊंगा","ाइयाँ","ाइयों","ाइयां","ाएगी","ाएगा","ाओगी","ाओगे","एंगी","ेंगी","एंगे","ेंगे","ूंगी","ूंगा","ातीं","नाओं","नाएं","ताओं","ताएं","यों","याँ","ाकर","ाइए","ाईं","ाया","ेगी","ेगा","ोगी","ोगे","ाने","ाना","ाते","ाती","ाता","तीं","ाओं","ाएं","ुओं","ुएं","ुआं","कर","ाओ","िए","ाई","ाए","ने","नी","ना","ते","ीं","ती","ता","ाँ","ां","ों","ें","ो","े","ू","ु","ी","ि","ा","एँ"]
 for word in sentence.split():
     #for ignore in lines:
         #if(word == ignore):
             #print(word)
             #counter=counter+1
             #break
-    #file=open("/home/tanmay/Desktop/stems.txt","r")
-    if(counter!=0):
-        counter=0
+    #file=open("/home/tanmay/Desktop/stems.txt","r") 
+    for ign in conso:
+        if ign == word:
+            print(word)
+            flag1=flag1+1
+            break
+    if flag1!=0:
+        flag1=0
         continue
     for end in lists:
-        if word.endswith(end):
-            for c in conso:
-                if(word.strip(end)==c):
-                    print(word)# not working on inflection "ाने" and बनाना type words
-                   # print(c)
-                   # print(word.strip(end))
-                    #print(end)
-                    flag2=flag2+1
-                    break
-            if(flag2==0):
-                print(word.strip(end))
-                flag=flag+1
-                break
-            if(flag2!=0):
-                break
-    if(flag==0 and flag2==0):
+        if (word.endswith(end) and len(word)>len(end)+1):
+            print(word[:-len(end)])
+            flag2=flag2+1
+            break
+            
+    if (flag2==0):
         print(word)
-    flag=0
     flag2=0
-#file1.close()
-file2.close()    
